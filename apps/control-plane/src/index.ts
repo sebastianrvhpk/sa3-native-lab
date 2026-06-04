4303b206dfe40eda7eb1c785783a15650e977d45
sebastian <sebastianrv.hpk@gmail.com>
2026-06-04 13:49:05 -0400

    Remove abandoned app surfaces

--- apps/control-plane/src/index.ts
+++
@@ -1,4 +0,0 @@
-export type { AppRouter } from "./router.js";
-export { appRouter } from "./router.js";
-export type { ResultFamily, WorkbenchState } from "./workbench.js";
-export { buildResultFamilies, loadWorkbenchState, shapeWorkbenchState } from "./workbench.js";
